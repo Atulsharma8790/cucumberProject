b8c9b257f8d8c0e1c0af3fecec60ae5efa4c8da6
Atul Sharma <atulsharma@Atuls-MacBook-Pro.local>
2017-09-20 13:11:49 +0530

    Code updated for cucumber learning

--- com.cucumber.project/src/test/java/cucumberTests/firstTest.java
+++ com.cucumber.project/src/test/java/cucumberTests/firstTest.java
@@ -20,21 +20,45 @@ public class firstTest {
 
 	@Given("^This is my first dummy step$")
 	public void this_is_my_first()  {
-	  System.out.println("1st dummy");
+		System.out.println("1st dummy");
 	}
 
 	@When("^This is my second dummy step$")
 	public void this_is_my_second(){	    
-		
+
 		System.out.println("Second Step");
-	
+
 	}
 
 	@Then("This is my third dummy steps")
 	public void this_is_my_third(DataTable stepNumber) {
-		
+
 		List<Map<String, String>> data = stepNumber.asMaps(String.class, String.class);
-	System.out.println("third step: "+data.get(0).get("stepNumber"));
+		for(int i=0;i<data.size();i++)
+			System.out.println("third step: "+data.get(i).get("stepNumber"));
+	}
+
+
+	@Given("^My Name is \"([^\"]*)\"$")
+	public void my_Name_is(String arg1) throws Throwable {
+		System.out.println("My Name Is: " + arg1);
+	}
+
+	@When("^I am a \"([^\"]*)\"$")
+	public void i_am_a(String arg1) throws Throwable {
+		System.out.println("I am a: " + arg1);
+
+	}
+
+	@When("^I am \"([^\"]*)\"$")
+	public void i_am(String arg1) throws Throwable {
+		System.out.println("I am : " + arg1);
+
+	}
+
+	@Then("^This is all about me$")
+	public void this_is_all_about_me() throws Throwable {
+		System.out.println("Thank You");
 	}
 
 
--- com.cucumber.project/target/test-classes/myFeature.feature
+++ com.cucumber.project/target/test-classes/myFeature.feature
@@ -7,3 +7,23 @@ Scenario: This is my dummy scenario
 	|StepNumber1|
 	|StepNumber2|
 	|StepNumber3|
+	
+Scenario: This is scenario for Atul
+	Given My Name is "Atul"
+	When I am a "software Test Lead"
+	And I am "Working Professional"	
+	Then This is all about me
+	
+Scenario: This is scenario for Shikha
+	Given My Name is "Shikha"
+	When I am a "software Developer Java"
+	And I am "Working Professional"	
+	Then This is all about me
+	
+Scenario: This is scenario for Scooby
+	Given My Name is "Scooby"
+	When I am a "Doggy bhow bhow"
+	And I am "everyone's favourite"	
+	Then This is all about me
+	
+	
